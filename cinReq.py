df8c61cef05439f4d16d5b0911ae0edc4d741fac
maw1395 <maw13r@my.fsu.edu>
2018-10-24 14:54:01 -0400

    Added iterative structure to iterate over all games from 1980 - 2018

--- cinReq.py
+++ cinReq.py
@@ -3,8 +3,42 @@ from bs4 import BeautifulSoup, Comment
 
 teamdict = {
 				"ATL": "AtlantaBraves",
-				"NYM": "NewYorkMets"
+				"NYM": "NewYorkMets",
 			}
+def Teams():
+	TeamsUrls=[]
+	for year in range(1980, 2018):
+		cont = get("https://www.baseball-reference.com/leagues/MLB/" + str(year) + ".shtml").content
+		soup = BeautifulSoup(cont, "lxml")
+		TeamTable=soup.find("table",{"class": "sortable stats_table", "id": "teams_standard_batting"} )
+		Teams = TeamTable.findAll("th", {"class": "left"})
+		
+		for Team in Teams:
+			try:
+				TeamsUrls.append(Team.find("a").get('href'))
+			except:
+				continue
+		#print TeamsUrls
+	return TeamsUrls
+
+def Games():
+	teams=Teams()
+	TotalGames=[]
+	for i in teams:
+		cont = get("https://www.baseball-reference.com/" + i). content
+		soup = BeautifulSoup(cont, "lxml")
+		Games = soup.findAll("ul", {"class": "timeline"})
+		
+		for j in Games:
+			Gamesj = j.findAll("li", {"class" : "result"})
+		#print Games
+
+			for game in Gamesj:
+				TotalGames.append(game.find("a").get('href'))
+	return TotalGames
+	#break
+
+
 
 def batter_info(batter, team_key, cont):
 	for x in batter:
@@ -41,80 +75,85 @@ def batter_info(batter, team_key, cont):
 			except:
 				pass
 
+def Pitcher_info():
+	GamesList = Games()
+	for Game in GamesList:
+		cont = get("https://www.baseball-reference.com/"+Game+".shtml").content
+		soup = BeautifulSoup(cont, "lxml")
+		comment = soup.find(text=lambda n: isinstance(n, Comment) and 'id="play_by_play"' in n)
+		soup2 = BeautifulSoup(comment, "lxml")
+		table = soup2.find("table",{"id": "play_by_play"} )
 
-cont = get("https://www.baseball-reference.com/boxes/NYN/NYN201704030.shtml").content
-soup = BeautifulSoup(cont, "lxml")
-comment = soup.find(text=lambda n: isinstance(n, Comment) and 'id="play_by_play"' in n)
-soup2 = BeautifulSoup(comment, "lxml")
-table = soup2.find("table",{"id": "play_by_play"} )
+		starting1 = ""     #starting pitcher 1
+		starting2 = ""     #starting pitcher 2
+		st1f = 0           #starting pitcher flag 1
+		st2f = 0           #starting pitcher flag 2
+		currentp = ""      #current pitcher
+		currentb = ""	   #current batter
+		currentbt= ""      #current batting team
 
-starting1 = ""     #starting pitcher 1
-starting2 = ""     #starting pitcher 2
-st1f = 0           #starting pitcher flag 1
-st2f = 0           #starting pitcher flag 2
-currentp = ""      #current pitcher
-currentb = ""	   #current batter
-currentbt= ""      #current batting team
+		p1s = 0            #pitcher 1 strikes
+		p1b = 0            #balls
+		p1t = 0            #total
 
-p1s = 0            #pitcher 1 strikes
-p1b = 0            #balls
-p1t = 0            #total
-
-p2s = 0            #pitcher 2 strikes
-p2b = 0            #balls
-pst = 0            #total
-
-
-for row in table.findAll("tr"):
-
-	cells1 = row.findAll("td", {"data-stat": "pitcher"})
-	for z in cells1:
-		hold = str(z).split("pitcher\">")[1].split("<")[0]
-
-		if st1f == 0:
-			starting1 = hold
-			st1f = 1
-		if starting1 != hold and st2f == 0:
-			starting2 = hold
-			st2f = 1
-		if starting1 == hold or starting2 == hold:
-			print hold
-		else:
-			exit()
-
-	cells2 = row.findAll("td", {"data-stat": "outs"})
-
-	for z in cells2:
-		out = str(z).split("outs\">")[1].split("<")[0]
-		if out == "":
-			break
-		else:
-			print "out:" + out
-
-	cells3 = row.findAll("th", {"data-stat": "inning"})
-
-	for z in cells3:
-		inning = str(z).split("\">")[1].split("<")[0]
-		if inning == "":
-			break
-		else:
-			print "inning:" + inning
-
-	cells4 = row.findAll("td", {"data-stat": "pitches_pbp"})
-	for i in cells4:
-		print str(i).split("p\">")[1].split("<spa")[0]
+		p2s = 0            #pitcher 2 strikes
+		p2b = 0            #balls
+		pst = 0            #total
 
 
-	cells5 = row.findAll("td", {"data-stat": "play_desc"})
-	for i in cells5:
-		print str(i).split("desc\">")[1].split("<")[0]
+		for row in table.findAll("tr"):
 
-	cells6 = row.findAll("td", {"data-stat": "batting_team_id"})
-	for i in cells6:
-		currentbt = str(i).split("\">")[1].split("<")[0]
-		print currentbt
+			cells1 = row.findAll("td", {"data-stat": "pitcher"})
+			for z in cells1:
+				hold = str(z).split("pitcher\">")[1].split("<")[0]
 
-	cells7 = row.findAll("td", {"data-stat": "batter"})
-	for i in cells7:
-		currentb = str(i).split("\">")[1].split("<")[0]
-		batter_info(currentb, currentbt, cont)
+				if st1f == 0:
+					starting1 = hold
+					st1f = 1
+				if starting1 != hold and st2f == 0:
+					starting2 = hold
+					st2f = 1
+				if starting1 == hold or starting2 == hold:
+					print hold
+				else:
+					exit()
+
+			cells2 = row.findAll("td", {"data-stat": "outs"})
+
+			for z in cells2:
+				out = str(z).split("outs\">")[1].split("<")[0]
+				if out == "":
+					break
+				else:
+					print "out:" + out
+
+			cells3 = row.findAll("th", {"data-stat": "inning"})
+
+			for z in cells3:
+				inning = str(z).split("\">")[1].split("<")[0]
+				if inning == "":
+					break
+				else:
+					print "inning:" + inning
+
+			cells4 = row.findAll("td", {"data-stat": "pitches_pbp"})
+			for i in cells4:
+				print str(i).split("p\">")[1].split("<spa")[0]
+
+
+			cells5 = row.findAll("td", {"data-stat": "play_desc"})
+			for i in cells5:
+				print str(i).split("desc\">")[1].split("<")[0]
+
+			cells6 = row.findAll("td", {"data-stat": "batting_team_id"})
+			for i in cells6:
+				currentbt = str(i).split("\">")[1].split("<")[0]
+				print currentbt
+
+			cells7 = row.findAll("td", {"data-stat": "batter"})
+			for i in cells7:
+				currentb = str(i).split("\">")[1].split("<")[0]
+				batter_info(currentb, currentbt, cont)
+
+if __name__ == "__main__":
+	main()
