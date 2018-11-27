20ae11d9eb907caadcbe6403e3f879e16443d93d
maw1395 <maw13r@my.fsu.edu>
2018-11-26 20:11:51 -0500

    sql indexing

--- SQLTEST.py
+++ SQLTEST.py
@@ -1,55 +1,51 @@
-import sqlite3
+import sqlalchemy as db
+import collections
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from matplotlib import rcParams
 
-sqlite_file = 'project.db'
-table_name = 'PlateAppearance'
-id1="Team" 
-id2="Year"
-id3="Game_Number" 
-id4="Pitcher" 
-id5="Strike" 
-id6="Ball" 
-id7="Pitches" 
-id8="Outs" 
-id9="Inning" 
-id10="Slugging" 
-id11="Obp" 
-id12="Ops" 
-id13="War" 
-id14="Hand" 
-id15="Outcome" 
+engine = db.create_engine('sqlite:///project.db')
+connection = engine.connect()
+metadata = db.MetaData()
+PlateAppearance = db.Table('PlateAppearance', metadata, autoload=True, autoload_with=engine)
 
-f1="ARZ"
-f2=1
-f3=1
-f4="BET"
-f5=1
-f6=1
-f7=1
-f8=1
-f9=1
-f10=1
-f11=1
-f12=1
-f13=1
-f14="L"
-f15="hit"
-
-
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-
-try:
-    c.execute("INSERT INTO {tn} ({i1},  {i2}, {i3},  {i4}, {i5},  {i6}, {i7},  {i8},\
-     {i9},  {i10}, {i11},  {i12}, {i13},  {i14}, {i15}) VALUES\
-     ({fi1},  {fi2}, {fi3},  {fi4}, {fi5},  {fi6}, {fi7},  {fi8},\
-     {fi9},  {fi10}, {fi11},  {fi12}, {fi13},  {fi14}, {fi15})".\
-     format(
-tn= table_name, i1=id1, i2=id2, i3=id3, i4=id4, i5=id5, i6=id6, i7=id7, i8=id8, i9=id9, i10=id10,
-i11=id11, i12=id12, i13=id13, i14=id14, i15=id15,
-fi1=f1, fi2=f2, fi3=f3, fi4=f4, fi5=f5, fi6=f6, fi7=f7, fi8=f8, fi9=f9, fi10=f10,
-fi11=f11, fi12=f12, fi13=f13, fi14=f14, fi15=f15))
-except sqlite3.IntegrityError:
-    print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
-
-conn.commit()
-conn.close()
+INNINGS={}
+stmt = "select Game_Number, Pitcher, outs, max(inning), outcome from PlateAppearance group by game_number, pitcher, outs"
+Pitcher = (connection.execute(stmt).fetchall())
+game=Pitcher[0][0]
+startingPitcher=Pitcher[0][1]
+inning=[]
+for i in Pitcher:
+	if(startingPitcher!=i[1]):
+		startingPitcher=i[1]
+		inningPlusOuts = max(inning) 
+		inning=[]
+		if inningPlusOuts not in INNINGS:
+			INNINGS[inningPlusOuts]=1
+		else:
+			INNINGS[inningPlusOuts]+=1	
+	thisout=i[2]
+	if "Flyball" in i[4] or "out" in i[4]:
+		 thisout+=1
+	elif "Double Play" in i[4]:
+		thisout+=2
+	elif "Triple Play" in i[4]:
+		thisout+=3
+	else:
+		thisout=i[2]
+	inning.append(i[3]+thisout*.1)
+print INNINGS
+outlist=[]
+for key in sorted(INNINGS.iterkeys()):
+     outlist.append(INNINGS[key])
+     print key
+     
+inningslist=range(1000,1036)
+plt.bar(inningslist, outlist,color='black', align='center')
+plt.xlabel("Inning")
+plt.ylabel("Pulled")
+plt.xticks(inningslist, ( '1.0', '1.1', '1.2', '1.3', '2.0', '2.1', '2.2', '2.3', '3.0', '3.1', '3.2', '3.3', '4.0', '4.1', '4.2', '4.3', '5.0', '5.1', '5.2', '5.3', '6.0', '6.1', '6.2', '6.3', '7.0', '7.1', '7.2', '7.3', '8.0', '8.1', '8.2', '8.3', '9.0', '9.1', '9.2', '9.3'))
+plt.title("2000-2009 Pitchers Pulled By Inning and Outs")
+rcParams.update({'figure.autolayout': True})
+#plt.savefig("Pulled-By-Inning.pdf")
+plt.show()
